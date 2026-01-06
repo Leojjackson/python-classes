753e6ac659f1936e7ba4c311f83d3f7d00390d8f
Leonardo Jackson <leojjackson@outlook.com>
2026-01-06 16:53:25 -0300

    Programa Quase no fim

--- velocidade-carros.py
+++ velocidade-carros.py
@@ -1,9 +1,16 @@
 # Crie um programa que calcule a velocidade de um carro na rodovia
-# onde o permitido é 90km/h nos finais de semana, na semana o limite é de 70km/h
-# com multa de 9$ por km ultrapassado.
+# onde o permitido é 90km/h com multa de 9$ por km excedido.
 
 
 print("Verificador de Velocidade")
 
-print("Limite permitido FINAL DE SEMANA (SEX/SAB/DOM) = 90KM/H")
-print("Limite permitido DIAS DA SEMANA (SEG/TER/QUA/QUI) = 70KM/H")
+print("Limite permitido = 90KM/H")
+
+velocidade = int(input("Qual a velocidade do carro? "))
+
+if velocidade < 90:
+    print("Você não Ultrapassou o limite da via. Sem Multas!!!")
+else:
+    multa = (velocidade - 90) * 9
+    print("Você Ultrapassou o limite da via. Multado!!!")
+    print("O limite permitido é de \033[32m{}KM/H\033[m, Sua velocidade foi de \033[31m{}KM/H\033[m, Você Foi multado no valor de \033[31m{}\033[m.".format(90, velocidade, multa))
