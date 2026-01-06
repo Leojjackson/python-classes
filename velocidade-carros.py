73ea0394959e70c96e1b16387d820b3e9b8efe2e
Leonardo Jackson <leojjackson@outlook.com>
2026-01-06 16:41:12 -0300

    Programa iniciado

---
+++ velocidade-carros.py
@@ -0,0 +1,9 @@
+# Crie um programa que calcule a velocidade de um carro na rodovia
+# onde o permitido é 90km/h nos finais de semana, na semana o limite é de 70km/h
+# com multa de 9$ por km ultrapassado.
+
+
+print("Verificador de Velocidade")
+
+print("Limite permitido FINAL DE SEMANA (SEX/SAB/DOM) = 90KM/H")
+print("Limite permitido DIAS DA SEMANA (SEG/TER/QUA/QUI) = 70KM/H")
