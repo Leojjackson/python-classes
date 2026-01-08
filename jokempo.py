6bcd5f4b2252c347f825f6278d22d146063107e4
Leonardo Jackson <leojjackson@outlook.com>
2026-01-08 10:44:40 -0300

    Numero aleatório encontrado, função da maquina finalizado

--- jokempo.py
+++ jokempo.py
@@ -2,12 +2,16 @@ import random
 
 # Jogo de Jo kem Po
 
-jokempo = ['papel', 'tesoura', 'pedra']
+jokempo = ['Pedra', 'Papel', 'Tesoura']
 aleatorio = random.randint(0,2)
+jokempo = jokempo[aleatorio]
 print("Jogo De Jo-kem-po")
+print(jokempo)
 print('''
         [ 1 ] Pedra
         [ 2 ] Papel
         [ 3 ] Tesoura''')
 escolha = int(input("Digite o numero de sua escolha: "))
+
+
 
