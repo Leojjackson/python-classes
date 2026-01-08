876719e389f56d74fe7bd7d8c9b22d445c228781
Leonardo Jackson <leojjackson@outlook.com>
2026-01-08 18:00:37 -0300

    Jogo Finalizado e Funcionando

--- jokempo.py
+++ jokempo.py
@@ -7,9 +7,9 @@ jokempo = ['Pedra', 'Papel', 'Tesoura']
 computador = random.randint(0,2)
 print("Jogo De Jo-kem-po")
 print(''' Suas Opções:
-        [ 1 ] Pedra
-        [ 2 ] Papel
-        [ 3 ] Tesoura
+        [ 0 ] Pedra
+        [ 1 ] Papel
+        [ 2 ] Tesoura
         ''')
 jogador = int(input("Qual é a sua jogada? :"))
 print("Jo")
@@ -20,35 +20,32 @@ print("PO")
 print('-='*15)
 print("Computador Jogou: {}".format(jokempo[computador]))
 print("Jogador jogou: {}".format(jokempo[jogador]))
-print('-='*20)
+print('-='*15)
 
-if computador == 0:
+if computador == 0: #pedra
     if jogador == 0:
         print("Empate")
     elif jogador == 1:
         print("JOGADOR VENCE")
-    if jokempo == 2:
+    if jogador == 2:
         print("COMPUTADOR VENCE")
-    else:
-        print('Jogada Invalida')
-elif computador == 1:
+
+elif computador == 1: #papel
     if jogador == 0:
         print("COMPUTADOR VENCE")
     elif jogador == 1:
         print('EMPATE')
-    if jokempo == 2:
+    if jogador == 2:
         print("JOGADOR VENCE")
-    else:
-        print('Jogada Invalida')
-elif computador == 2:
+
+elif computador == 2: #tesoura
     if jogador == 0:
         print("JOGADOR VENCE")
     elif jogador == 1:
         print('COMPUTADOR VENCE')
-    if jokempo == 2:
+    if jogador == 2:
         print("EMPATE")
-    else:
-        print('Jogada Invalida')
+
 
 
 
