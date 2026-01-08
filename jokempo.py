7c1353c74ca24da22ad485738d1c2e68ade5c253
Leonardo Jackson <leojjackson@outlook.com>
2026-01-08 11:16:19 -0300

    Faltam alguns resjustes ainda

--- jokempo.py
+++ jokempo.py
@@ -1,17 +1,54 @@
 import random
+from time import sleep
 
 # Jogo de Jo kem Po
 
 jokempo = ['Pedra', 'Papel', 'Tesoura']
-aleatorio = random.randint(0,2)
-jokempo = jokempo[aleatorio]
+computador = random.randint(0,2)
 print("Jogo De Jo-kem-po")
-print(jokempo)
-print('''
+print(''' Suas Opções:
         [ 1 ] Pedra
         [ 2 ] Papel
-        [ 3 ] Tesoura''')
-escolha = int(input("Digite o numero de sua escolha: "))
+        [ 3 ] Tesoura
+        ''')
+jogador = int(input("Qual é a sua jogada? :"))
+print("Jo")
+sleep(1)
+print("KEM")
+sleep(1)
+print("PO")
+print('-='*15)
+print("Computador Jogou: {}".format(jokempo[computador]))
+print("Jogador jogou: {}".format(jokempo[jogador]))
+print('-='*20)
+
+if computador == 0:
+    if jogador == 0:
+        print("Empate")
+    elif jogador == 1:
+        print("JOGADOR VENCE")
+    if jokempo == 2:
+        print("COMPUTADOR VENCE")
+    else:
+        print('Jogada Invalida')
+elif computador == 1:
+    if jogador == 0:
+        print("COMPUTADOR VENCE")
+    elif jogador == 1:
+        print('EMPATE')
+    if jokempo == 2:
+        print("JOGADOR VENCE")
+    else:
+        print('Jogada Invalida')
+elif computador == 2:
+    if jogador == 0:
+        print("JOGADOR VENCE")
+    elif jogador == 1:
+        print('COMPUTADOR VENCE')
+    if jokempo == 2:
+        print("EMPATE")
+    else:
+        print('Jogada Invalida')
 
 
 
